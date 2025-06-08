01ea4443f3c6e333fb28fa1b710437dec20e447e
eleonorakuteva <eleonorakuteva@gmail.com>
2025-06-08 18:59:25 +0300

    update ui.py

--- ui.py
+++ ui.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 
+from logic import GameLogic
+
 BACKGROUND = "#DFE4EA"
 BACKGROUND_PNG = "assets/background.png"
 TITLE_PNG = "assets/title_img.png"
@@ -9,6 +11,9 @@ TITLE_PNG = "assets/title_img.png"
 
 class GuesserApp:
     def __init__(self, window):
+        self.logic = GameLogic()
+        self.current_row = 0
+
         self.window = window
         self.window.title("Guess the secret four-digit number")
         self.window.geometry("400x600")
@@ -41,7 +46,7 @@ class GuesserApp:
         self.button_info = Button(self.window, text="Info", width=12, command=self.info_command)
         self.button_info.grid(row=0, column=2, padx=5, pady=5)
 
-        self.button_search = Button(self.window, text="Submit", width=20, command=self.search_command)
+        self.button_search = Button(self.window, text="Submit", width=20, command=self.iterate_over_rows)
         self.button_search.grid(row=14, column=1)
 
 
@@ -83,25 +88,69 @@ class GuesserApp:
 
 
 
-    def search_command(self):
-        print("Submit clicked!")
-        given_guess: str = self.guess_entry.get()
-        print(given_guess)
-        print(len(given_guess))
-        print(type(given_guess))
+    def is_guess_correct(self, given_guess):
+        """
+        Checks for correct guess entry.
+        :return: True
+        """
 
-        # Try / Except block:
-        if len(given_guess) != 4:
-            messagebox.showwarning(title= 'Oops', message='The number you are trying to guess is with four digit!')
-        if int(given_guess) not in range(1000,9999):
-            messagebox.showwarning(title= 'Oops', message='The number you are trying to guess is with four digit!')
+        # checks for digits
+        is_not_digit = False
+        for element in range(len(given_guess)):
+            if not given_guess[element].isdigit():
+                is_not_digit = True
+        if is_not_digit:
+            messagebox.showwarning(title="Oops", message="You have to guess four-digit NUMBER.\nLetters not allowed !")
+            self.guess_entry.delete(0, END)
+
+        # checks for length
+        elif len(given_guess) != len(self.logic.secret_number):
+            messagebox.showwarning(title='Oops',
+                                   message=f'The number you are trying to guess must be with length of {len(self.logic.secret_number)} digits!')
+            self.guess_entry.delete(0, END)
+
+        # checks for leading zeros:
+        elif given_guess[0] == "0":
+            messagebox.showwarning(title="Oops",
+                                   message=f"{given_guess} is not a valid four-digit number because in mathematics, leading zeroes do not count.\n"
+                                           f"A true four-digit number must be between 1000 and 9999.")
+            self.guess_entry.delete(0, END)
         else:
-            print(given_guess)
-        # for current_row in range(10):
-        #
-        #     self.guess_labels[current_row][0].config(text="1234")
-        #     self.guess_labels[current_row][1].config(text="2")
-        #     self.guess_labels[current_row][2].config(text="1")
+            self.guess_entry.delete(0, END)
+            return True
+
+
+
+
+    # def get_current_guess(self):
+
+    #     a_current_guess:str = self.guess_entry.get()
+    #     print(a_current_guess, type(a_current_guess)) #class "str"
+    #     return a_current_guess
+
+
+    def iterate_over_rows(self):
+
+        given_guess: str = self.guess_entry.get()
+
+        if self.current_row > 10:
+            messagebox.showwarning(title='Oops',
+                                   message=f'You lose! ')
+
+        if self.is_guess_correct(given_guess) and self.current_row <= 10:
+
+            print("its correct")
+
+            self.guess_labels[self.current_row][0].config(text=f"{given_guess}")
+            self.guess_labels[self.current_row][1].config(text="2")
+            self.guess_labels[self.current_row][2].config(text="1")
+            self.current_row += 1
+
+
+
+
+
+
 
     def hint_command(self):
         print("Hint clicked")
@@ -109,3 +158,18 @@ class GuesserApp:
     def info_command(self):
         print("Info clicked")
 
+
+
+
+
+
+
+window_tab = Tk()
+app = GuesserApp(window_tab)
+
+curr_guess = app.guess_entry.get()
+
+
+
+
+window_tab.mainloop()
